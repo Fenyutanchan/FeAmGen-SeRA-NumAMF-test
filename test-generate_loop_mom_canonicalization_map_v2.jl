f9eaf087ad36c8650b6e8d481958008d7f96b90b
Quan-feng WU <wuquanfeng@ihep.ac.cn>
2023-01-29 13:08:05 +0800

    done the test for test-generate_loop_mom_canonicalization_map_v2

--- test-generate_loop_mom_canonicalization_map_v2.jl
+++ test-generate_loop_mom_canonicalization_map_v2.jl
@@ -9,7 +9,7 @@ include("normalize_loop_mom.jl")
 
 n_den_tot   =   10
 n_leg_tot   =   4
-n_loop      =   3
+n_loop      =   5
 
 q_str_list  =   ["q$ii" for ii ∈ 1:n_loop]
 k_str_list  =   ["k$ii" for ii ∈ 1:n_leg_tot]
@@ -31,13 +31,11 @@ qi_list =   [Basic(q_str) for q_str ∈ q_str_list]
 k_list  =   [Basic(k_str) for k_str ∈ k_str_list]
 
 loop_den_list   =   [
-    Den(k1 - k2 + k3 - q1 + q2, m1, ieta),
-    Den(-k1 - k2 - k3 + q1, 0, 0),
-    Den(k3 + q3, 0, 0),
-    Den(-k1 - k2 - k3 + q3, m4, ieta),
-    Den(-k2 + k3 - q1 + q2, 0, ieta),
-    Den(-k1 + k2 - k3 + q3, 0, ieta),
-    Den(k1 - k2 + k3 + q2, m7, 0)
+    Den(-k1 + q2 - q4, 0, 0),
+    Den(k1 - q1 + q3, m2, 0),
+    Den(-q4 + q5, 0, 0),
+    Den(-k1 - q2 - q3, m4, ieta),
+    Den(k1 - q1 + q2, 0, ieta)
 ]
 
 vanish_qi_dict  =   Dict{Basic, Basic}(qi_list .=> zero(Basic))
@@ -47,8 +45,8 @@ den_ext_mom_list    =   subs.(den_mom_list, Ref(vanish_qi_dict))
 vac_den_mom_list    =   expand.(den_mom_list - den_ext_mom_list)
 unique!(vac_den_mom_list)
 
-opposite_sign_pair_list =   Vector{Tuple{Basic, Basic}}()
 same_sign_pair_list     =   Vector{Tuple{Basic, Basic}}()
+opposite_sign_pair_list =   Vector{Tuple{Basic, Basic}}()
 for qi_index ∈ 1:(n_loop-1)
     qi  =   qi_list[qi_index]
     for qj_index ∈ (qi_index+1):n_loop
@@ -87,37 +85,46 @@ for qi_index ∈ 1:(n_loop-1)
     end # for qj_index
 end # for qi_index
 
-q1_same_sign_qi_list = Basic[q1]
-while true
-    pair_list   =   filter(
-        one_pair -> !(isempty ∘ intersect)(q1_same_sign_qi_list, one_pair),
+q1_same_sign_qi_list        =   Basic[q1]
+q1_opposite_sign_qi_list    =   Basic[]
+while !isempty(same_sign_pair_list) || !isempty(opposite_sign_pair_list)
+    q1_same_sign_pair_list   =   filter(
+        one_pair -> !(isempty ∘ intersect)(one_pair, q1_same_sign_qi_list),
         same_sign_pair_list
     )
-    if isempty(pair_list)
-        break # none in same_sign_pair_list is related to q1_same_sign_qi_list
-    end # if
-    union!(q1_same_sign_qi_list, pair_list...)
-    setdiff!(same_sign_pair_list, pair_list)
-end # while
+    union!(q1_same_sign_qi_list, q1_same_sign_pair_list...)
+    setdiff!(same_sign_pair_list, q1_same_sign_pair_list)
 
-pos_list    =   findall(
-    one_pair -> !(isempty ∘ intersect)(one_pair, q1_same_sign_qi_list),
-    opposite_sign_pair_list
-)
-q1_opposite_sign_qi_list    =   (
-    isempty(pos_list) ?
-    Vector{Basic}() : 
-    (first ∘ setdiff).(opposite_sign_pair_list[pos_list], Ref(q1_same_sign_qi_list))
-)
+    q1_opposite_sign_pair_list  =   filter(
+        one_pair -> !(isempty ∘ intersect)(one_pair, q1_same_sign_qi_list),
+        opposite_sign_pair_list
+    )
+    union!(
+        q1_opposite_sign_qi_list,
+        [
+            (first ∘ setdiff)(one_pair, q1_same_sign_qi_list)
+            for one_pair ∈ q1_opposite_sign_pair_list
+        ]
+    )
+    setdiff!(opposite_sign_pair_list, q1_opposite_sign_pair_list)
 
-while !isempty(q1_opposite_sign_qi_list)
-    pair_list   =   filter(
-        one_pair -> !(isempty ∘ intersect)(q1_opposite_sign_qi_list, one_pair),
+    q1_opposite_sign_pair_list  =   filter(
+        one_pair -> !(isempty ∘ intersect)(one_pair, q1_opposite_sign_qi_list),
         same_sign_pair_list
     )
-    if isempty(pair_list)
-        break # none in same_sign_pair_list is related to q1_opposite_sign_qi_list
-    end # if
-    union!(q1_opposite_sign_qi_list, pair_list...)
-    setdiff!(same_sign_pair_list, pair_list)
-end # while
+    union!(q1_opposite_sign_qi_list, q1_opposite_sign_pair_list...)
+    setdiff!(same_sign_pair_list, q1_opposite_sign_pair_list)
+
+    q1_same_sign_pair_list  =   filter(
+        one_pair -> !(isempty ∘ intersect)(one_pair, q1_opposite_sign_qi_list),
+        opposite_sign_pair_list
+    )
+    union!(
+        q1_same_sign_qi_list,
+        [
+            (first ∘ setdiff)(one_pair, q1_opposite_sign_qi_list)
+            for one_pair ∈ q1_same_sign_pair_list
+        ]
+    )
+    setdiff!(opposite_sign_pair_list, q1_opposite_sign_pair_list)
+end

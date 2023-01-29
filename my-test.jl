c958d2aa93ad4f441f8541d52253d6bc5c50265f
Quan-feng WU <wuquanfeng@ihep.ac.cn>
2023-01-29 19:06:57 +0800

    done for random generation of the case which allow disjoint loop momenta.

--- my-test.jl
+++ my-test.jl
@@ -11,8 +11,8 @@ include("generate_loop_mom_canonicalization_map_v2.jl")
 include("normalize_loop_mom.jl")
 
 n_den_tot   =   10
-n_leg_tot   =   4
-n_loop_tot  =   6
+n_leg_tot   =   10
+n_loop_tot  =   10
 
 q_str_list  =   ["q$ii" for ii ∈ 1:n_loop_tot]
 k_str_list  =   ["k$ii" for ii ∈ 1:n_leg_tot]
@@ -81,54 +81,69 @@ for n_den ∈ 1:n_den_tot, n_leg ∈ 1:n_leg_tot, n_loop ∈ 1:n_loop_tot
         q1_same_sign_qi_list
     )
     
-    non_trivial_no_intersection_flag    =   if isempty(q1_opposite_sign_qi_list)
-        false
-    else
-        have_q1_same_sign_qi_list       =   [
-            (isempty ∘ findall)(!iszero, coeff.(mom, q1_same_sign_qi_list))
-            for mom ∈ mom_list
-        ]
-        have_q1_opposite_sign_qi_list   =   [
-            (isempty ∘ findall)(!iszero, coeff.(mom, q1_opposite_sign_qi_list))
-            for mom ∈ mom_list
-        ]
+    # non_trivial_no_intersection_flag    =   if isempty(q1_opposite_sign_qi_list)
+    #     false
+    # else
+    #     have_q1_same_sign_qi_list       =   [
+    #         (isempty ∘ findall)(!iszero, coeff.(mom, q1_same_sign_qi_list))
+    #         for mom ∈ mom_list
+    #     ]
+    #     have_q1_opposite_sign_qi_list   =   [
+    #         (isempty ∘ findall)(!iszero, coeff.(mom, q1_opposite_sign_qi_list))
+    #         for mom ∈ mom_list
+    #     ]
 
-        all(
-            xor.(
-                have_q1_same_sign_qi_list,
-                have_q1_opposite_sign_qi_list
-            )
-        )
-    end
+    #     all(
+    #         xor.(
+    #             have_q1_same_sign_qi_list,
+    #             have_q1_opposite_sign_qi_list
+    #         )
+    #     )
+    # end
 
     q_indices_partition, mom_indices_partition   =   disjoint_loop_momenta_partition(
         q_list[1:n_loop], mom_list
     )
 
-    for ii ∈ 1:n_den
-        mom     =   mom_list[ii]
-        k_sum   =   subs(
-            mom,
-            Dict{Basic, Basic}(
-                [Basic("q$jj") => zero(Basic) for jj ∈ 1:n_loop]
+    for (this_q_partition, this_mom_partition) ∈ zip(q_indices_partition, mom_indices_partition)
+        this_q_list     =   q_list[this_q_partition]
+        this_mom_list   =   mom_list[this_mom_partition]
+
+        this_base_sign              =   q_sign_list[first(this_q_partition)]
+        this_same_sign_qi_list      =   this_q_list[
+            findall(
+                q_sign -> q_sign * this_base_sign > 0,
+                q_sign_list[this_q_partition]
             )
+        ]
+        this_opposite_sign_qi_list  =   setdiff(
+            this_q_list,
+            this_same_sign_qi_list
         )
 
-        bench_sign, q_sum_bench =   begin
-            q_coeff_list    =   coeff.(mom, q_list[1:n_loop])
-            first_qi_index  =   findfirst(!iszero, q_coeff_list)
-            @assert !isnothing(first_qi_index)
-            first_qi        =   q_list[first_qi_index]
-            (
-                non_trivial_no_intersection_flag ? 
-                q_coeff_list[first_qi_index] :
-                (first_qi ∈ q1_same_sign_qi_list ? 1 : -1) * q_coeff_list[first_qi_index]
-            ), sum(
-                q_list[findall(!iszero, q_coeff_list)]
+        for this_mom_index ∈ eachindex(this_mom_list)
+            mom     =   this_mom_list[this_mom_index]
+            k_sum   =   subs(
+                mom,
+                Dict{Basic, Basic}(
+                    [qi_ => zero(Basic) for qi_ ∈ this_q_list]
+                )
+            )
+
+            bench_sign, q_sum_bench =   begin
+                q_coeff_list        =   coeff.(mom, this_q_list)
+                this_first_qi_index =   findfirst(!iszero, q_coeff_list)
+                @assert !isnothing(this_first_qi_index)
+                first_qi            =   this_q_list[this_first_qi_index]
+                (first_qi ∈ this_same_sign_qi_list ? 1 : -1) * q_coeff_list[this_first_qi_index], sum(
+                    this_q_list[findall(!iszero, q_coeff_list)]
+                )
+            end
+
+            mom_list_bench[this_mom_partition[this_mom_index]]  =   expand(
+                q_sum_bench + bench_sign * k_sum
             )
         end
-
-        mom_list_bench[ii]  =   expand(q_sum_bench + bench_sign * k_sum)
     end
 
     loop_den_list       =   Den.(mom_list, m_list, ieta_list)
@@ -155,7 +170,7 @@ for n_den ∈ 1:n_den_tot, n_leg ∈ 1:n_leg_tot, n_loop ∈ 1:n_loop_tot
         println()
         println(loop_den_list_v2)
         println()
-        println(non_trivial_no_intersection_flag)
+        # println(non_trivial_no_intersection_flag)
         println(loop_den_list_bench)
     end
 end
